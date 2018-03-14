f0aca829db243bcef52090ecc89d068d83994599
Andrew Eakett <aeakett@gmail.com>
2018-03-14 15:11:01 -0400

    add Grunt-based building of favicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,82 @@ module.exports = function(grunt) {
       '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
       'Andrew Eakett */\n',
     // Task configuration.
+    realFavicon: {
+      favicons: {
+        src: 'img/src/favicon.png',
+        dest: './',
+        options: {
+          iconsPath: '/ttt/',
+          html: [ 'index.htm' ],
+          design: {
+            ios: {
+              pictureAspect: 'noChange',
+              assets: {
+                ios6AndPriorIcons: true,
+                ios7AndLaterIcons: true,
+                precomposedIcons: true,
+                declareOnlyDefaultIcon: false
+              },
+              appName: 'Tappy-Tap-Tap'
+            },
+            desktopBrowser: {},
+            windows: {
+              masterPicture: {
+                content: 'img/src/favicon-outline.png'
+              },
+              pictureAspect: 'whiteSilhouette',
+              backgroundColor: '#073642',
+              onConflict: 'override',
+              assets: {
+                windows80Ie10Tile: true,
+                windows10Ie11EdgeTiles: {
+                  small: true,
+                  medium: true,
+                  big: true,
+                  rectangle: true
+                }
+              },
+              appName: 'Tappy-Tap-Tap'
+            },
+            androidChrome: {
+              pictureAspect: 'noChange',
+              themeColor: '#073642',
+              manifest: {
+                name: 'Tappy-Tap-Tap',
+                startUrl: 'http://eakett.ca/ttt/',
+                display: 'standalone',
+                orientation: 'notSet',
+                onConflict: 'override',
+                declared: true
+              },
+              assets: {
+                legacyIcon: true,
+                lowResolutionIcons: true
+              }
+            },
+            safariPinnedTab: {
+              masterPicture: {
+                content: 'img/src/favicon-outline.png'
+              },
+              pictureAspect: 'silhouette',
+              themeColor: '#073642'
+            }
+          },
+          settings: {
+            compression: 5,
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+          },
+          versioning: {
+            paramName: 'v',
+            paramValue: '<%= grunt.template.today("yyyymmddhhMMssl") %>'
+          }
+        }
+      }
+    },
     uglify: {
       my_target: {
         options: {
@@ -44,8 +120,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-real-favicon');
 
   // Default task.
-  grunt.registerTask('default', ['sass', 'uglify']);
+  grunt.registerTask('default', ['sass', 'uglify', 'realFavicon']);
 
 };
